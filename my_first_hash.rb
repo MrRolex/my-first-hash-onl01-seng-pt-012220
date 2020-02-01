6debc2e901de10cc275dcb83c8880322278fa75f
Edward Williams <eddie_w38@yahoo.com>
2020-02-01 14:11:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,17 +1,15 @@
-def my_hash = {"my_name" => "Herman Melville"}
-  my_hash["my_name"]
+require 'pry'
 
-  
-  
+def my_hash
+  myhash = {"Name" => "Eddie"}
 end
-
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  the_manifest["wale bone corsets"]
+  
 
 
 end
@@ -35,13 +33,17 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets" => 2]
+ shipping_manifest = ["muskets"] = 2
+   end
+
 
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder" => 4]
+  
+shipping_manifest = ["gun powder"] = 4
+   end
 
 
   # return the shipping_manifest hash below
